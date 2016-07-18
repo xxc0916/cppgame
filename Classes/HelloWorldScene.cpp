fe7e9de58bb2b70104bebb009147995228700553
xxc0916 <758482981@qq.com>
2016-07-18 11:11:45 +0800

    节点的运用

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -30,49 +30,27 @@ bool HelloWorld::init()
     
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	auto back = Sprite::create("pan1.png");
+	back->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+	back->setScale(0.5);
+	addChild(back);
+	auto clipNode = ClippingNode::create(back);
 
-    ///////////////////////////////
-    //// 2. add a menu item with "X" image, which is clicked to quit the program
-    ////    you may modify it.
+	addChild(clipNode);
 
-    //// add a "close" icon to exit the progress. it's an autorelease object
-    //auto closeItem = MenuItemImage::create(
-    //                                       "CloseNormal.png",
-    //                                       "CloseSelected.png",
-    //                                       CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    //
-    //closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
-    //                            origin.y + closeItem->getContentSize().height/2));
+	clipNode->setInverted(false);
 
-    //// create menu, it's an autorelease object
-    //auto menu = Menu::create(closeItem, NULL);
-    //menu->setPosition(Vec2::ZERO);
-    //this->addChild(menu, 1);
+	clipNode->setAlphaThreshold(0.5);
 
-    ///////////////////////////////
-    //// 3. add your codes below...
+	auto light = Sprite::create("light.png");
 
-    //// add a label shows "Hello World"
-    //// create and initialize a label
-    //
-    //auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    //
-    //// position the label on the center of the screen
-    //label->setPosition(Vec2(origin.x + visibleSize.width/2,
-    //                        origin.y + visibleSize.height - label->getContentSize().height));
+	clipNode->addChild(light);
 
-    //// add the label as a child to this layer
-    //this->addChild(label, 1);
+	light->setPosition(Vec2(visibleSize.width / 2 - 100, visibleSize.height / 2));
 
-    //// add "HelloWorld" splash screen"
-    //auto sprite = Sprite::create("HelloWorld.png");
-
-    //// position the sprite on the center of the screen
-    //sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-    //// add the sprite as a child to this layer
-    //this->addChild(sprite, 0);
-    
+	light->runAction(RepeatForever::create(Sequence::create(MoveBy::create(2.0, Vec2(200, 0)),
+		MoveBy::create(0.01, Vec2(-200, 0))
+		, NULL)));
     return true;
 }
 
